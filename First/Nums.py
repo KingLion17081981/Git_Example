19253362236173423761ffcb43c1f85c0b2ffe57
Aleksander Smirnov <Kingleon17081981@gmail.com>
2025-01-28 11:18:45 +0300

    Добавлена функция "minus" и вывод на печать этой функции

--- First/Nums.py
+++ First/Nums.py
@@ -1,5 +1,11 @@
 def plus(a: int, b: int) -> int:
     return a + b
 
+text = "New text"
+
+def minus(a: int, b: int) -> int:
+    return a - b
+
 if __name__ == '__main__':
     print(plus(3, 2))
+    print(minus(5, 6))
